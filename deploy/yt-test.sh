a26bf49a283a1cd2e4fa5f05aa9d204122a1333f
Aleksandr Razumov <ernado@go-faster.org>
2024-01-27 19:23:06 +0300

    fix(deploy): correctly build yt-http-test

--- deploy/yt-test.sh
+++ deploy/yt-test.sh
@@ -4,7 +4,7 @@ source yt.sh
 
 pushd ytst
 export CGO_ENABLED=0
-CGO_ENABLED=0 go build ./cmd/yt-http-test
+CGO_ENABLED=0 go build ../cmd/yt-http-test
 yt write-file '//yt-http-test' < yt-http-test
 yt vanilla --tasks '{task={command="./yt-http-test";"file_paths"=[<executable=%true;>"//yt-http-test";];"layer_paths"=["//ubuntu-base-22.04-base-amd64.tar.gz";];"make_rootfs_writable"=%true;"job_count"=1;};}'
 popd
