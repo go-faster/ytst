83134a754e315c7cd7be58a04dab9fcdadedd9f5
Aleksandr Razumov <ernado@go-faster.org>
2024-01-28 16:07:03 +0300

    fix(deploy/yt-setup): use -N option for wget
    The -nc forces non-zero exit code.

--- deploy/yt-setup.sh
+++ deploy/yt-setup.sh
@@ -7,6 +7,6 @@ VER=22.04
 IMG=ubuntu-base-${VER}-base-amd64.tar.gz
 
 echo ">> Loading base layer"
-wget -O ${IMG} -nc https://cdimage.ubuntu.com/ubuntu-base/releases/${VER}/release/${IMG}
+wget -N https://cdimage.ubuntu.com/ubuntu-base/releases/${VER}/release/${IMG}
 LAYER=//${IMG}
 yt write-file ${LAYER} < ${IMG}
