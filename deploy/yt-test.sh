79908130893cb20e9d8be4f8f7007682f650af5f
Aleksandr Razumov <ernado@go-faster.org>
2024-01-29 22:02:11 +0300

    feat(yt-test): save stdeerr

--- deploy/yt-test.sh
+++ deploy/yt-test.sh
@@ -3,4 +3,6 @@ set -e
 export CGO_ENABLED=0
 go build ../cmd/yt-http-test
 yt write-file '//yt-http-test' < yt-http-test
-yt vanilla --tasks '{task={command="./yt-http-test";"file_paths"=[<executable=%true;>"//yt-http-test";];"layer_paths"=["//ubuntu-base-20.04.5-base-amd64.tar.gz";];"make_rootfs_writable"=%true;"job_count"=1;};}'
+yt vanilla \
+  --spec='{stderr_table_path="//tmp/yt-test-stderr";}' \
+  --tasks '{task={command="./yt-http-test --duration 10s";"file_paths"=[<executable=%true;>"//yt-http-test";];"layer_paths"=["//ubuntu-base-20.04.5-base-amd64.tar.gz";];"make_rootfs_writable"=%true;"job_count"=1;};}'
