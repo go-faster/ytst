286f080e494f2ff617b3ee56342b3a7519f58aeb
Aleksandr Razumov <ernado@go-faster.org>
2024-01-29 22:20:12 +0300

    feat(yt-test): set max_failed_job_count

--- deploy/yt-test.sh
+++ deploy/yt-test.sh
@@ -4,5 +4,5 @@ export CGO_ENABLED=0
 go build ../cmd/yt-http-test
 yt write-file '//yt-http-test' < yt-http-test
 yt vanilla \
-  --spec='{stderr_table_path="//tmp/yt-test-stderr";}' \
+  --spec='{stderr_table_path="//tmp/yt-test-stderr";max_failed_job_count=2;}' \
   --tasks '{task={command="./yt-http-test --duration 10s";"file_paths"=[<executable=%true;>"//yt-http-test";];"layer_paths"=["//ubuntu-base-20.04.5-base-amd64.tar.gz";];"make_rootfs_writable"=%true;"job_count"=1;};}'
