a9b9cb47826b88d0b691029f0ae1c2a6614758ec
Aleksandr Razumov <ernado@go-faster.org>
2024-01-29 05:36:15 +0300

    chore(deploy): one CGO_ENABLED is enough

--- deploy/yt-test.sh
+++ deploy/yt-test.sh
@@ -3,6 +3,6 @@ set -e
 source yt.sh
 
 export CGO_ENABLED=0
-CGO_ENABLED=0 go build ../cmd/yt-http-test
+go build ../cmd/yt-http-test
 yt write-file '//yt-http-test' < yt-http-test
 yt vanilla --tasks '{task={command="./yt-http-test";"file_paths"=[<executable=%true;>"//yt-http-test";];"layer_paths"=["//ubuntu-base-20.04.5-base-amd64.tar.gz";];"make_rootfs_writable"=%true;"job_count"=1;};}'
