cfe2f9d85766bd0739d9bac4314c842932b6bece
Aleksandr Razumov <ernado@go-faster.org>
2024-02-12 14:39:37 +0300

    feat(deploy): install cilium for kind

--- deploy/kind.sh
+++ deploy/kind.sh
@@ -7,6 +7,12 @@ CLUSTER=yt
 kind delete cluster --name ${CLUSTER} -q || true
 kind create cluster --name ${CLUSTER} --config kind.yml
 
+echo ">> Cilium"
+kubectl create ns cilium
+cilium install --version 1.14.5 -n cilium --set bpf.autoMount.enabled=false
+cilium hubble enable -n cilium
+cilium -n cilium status --wait
+
 echo ">> Cert-Manager"
 kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.13.3/cert-manager.yaml
 kubectl -n cert-manager rollout status --timeout=1m deployment cert-manager-webhook
