cf8f83e5c189eccd2ba48999fad49a62a1c02b2f
Aleksandr Razumov <ernado@go-faster.org>
2024-03-08 18:23:03 +0300

    fix(deploy): use ISO instead of IMG var

--- _deploy/minikube.sh
+++ _deploy/minikube.sh
@@ -1,17 +1,17 @@
 set -e
 
-IMG=${IMG:"minikube-amd64.iso"}
+ISO=${ISO:"minikube-amd64.iso"}
 
 # if IMG is not set, download the latest minikube image
-if [ -z "$IMG" ]; then
+if [ -z "$ISO" ]; then
     echo ">> Downloading latest minikube image"
     GH_BIN=$(which gh)
     if [ -x "$GH_BIN" ]; then
         echo ">>> Using gh to get latest minikube image"
         REPO="go-faster/minikube"
         version=$(gh release view -R "${REPO}" --json tagName --jq '.tagName')
-        IMG="minikube-amd64-${version}.iso"
-        echo ">>> Downloading $IMG"
+        ISO="minikube-amd64-${version}.iso"
+        echo ">>> Downloading ISO"
         wget --no-clobber "https://ytsaurus.hb.ru-msk.vkcs.cloud/minikube/minikube-amd64-${version}.iso"
     else
         echo ">>> gh command not found"
@@ -24,18 +24,17 @@ pushd minikube
 make out/minikube
 popd
 
-ISO_REALPATH=$(realpath "${IMG}")
-ISO="file://${ISO_REALPATH}"
+ISO_REALPATH=$(realpath "${ISO}")
 # NB: To set the default driver, use `minikube config set driver virtualbox` command.
 # Set LANG=en for VBoxManage, since VBoxManage output depends on system language
 # and minikube fails to parse translated output.
 export LANG=en
 echo ">> Deleting old Minikube"
 ./minikube/out/minikube delete
-echo ">> Starting Minikube (iso: $ISO)"
+echo ">> Starting Minikube (iso: ${ISO_REALPATH})"
 MEM=${MEM:-8g}
 CPU=${CPU:-4}
-./minikube/out/minikube start --memory="${MEM}" --cpus="${CPU}" --iso-url="${ISO}" --cni=cilium --container-runtime=porto --cache-images=false
+./minikube/out/minikube start --memory="${MEM}" --cpus="${CPU}" --iso-url="file://${ISO_REALPATH}" --cni=cilium --container-runtime=porto --cache-images=false
 
 echo ">> Cert-Manager"
 kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.13.3/cert-manager.yaml
