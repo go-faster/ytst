c9c79f30bf4cc85a96d0038cfdbdf05c02b3c68c
Aleksandr Razumov <ernado@go-faster.org>
2024-03-21 19:28:52 +0300

    feat(deploy): set topoligy in kind

--- _deploy/operator.sh
+++ _deploy/operator.sh
@@ -11,7 +11,7 @@ NAME=op
 echo ">> Operator"
 pushd yt-k8s-operator
 make docker-build docker-push OPERATOR_IMAGE="${IMG}" IMG="${IMG}":"${TAG}" OPERATOR_TAG="${TAG}"
-helm upgrade ${NAME} ./ytop-chart --install --namespace ${NS} --create-namespace --set controllerManager.manager.image.repository="${IMG}" --set controllerManager.manager.image.tag="${TAG}"
+helm upgrade ${NAME} ./ytop-chart --install --namespace ${NS} --create-namespace --values ../ytop.yml --set controllerManager.manager.image.repository="${IMG}" --set controllerManager.manager.image.tag="${TAG}"
 popd
 
 kubectl -n ${NS} rollout status --timeout=5m deployment ${NAME}-ytop-chart-controller-manager
